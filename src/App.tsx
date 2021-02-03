4a3f501992c2c8f3918ed74d26a11e3c7be2f733
eNezaLaptop <edisonneza@hotmail.com>
2021-02-03 19:07:11 +0100

    Added app title in translator

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import "./App.css";
+import { useTranslation } from "react-i18next";
 import LanguageSelect from "./components/languageSelector";
 import HomeComponent from "./components/home/home";
 
 export default function App() {
+  const { t } = useTranslation();
+  document.title = t('app_title');
+
   return (
     <div className="main">
       <div className="language-select">
