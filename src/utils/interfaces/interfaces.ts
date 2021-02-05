ddd947a6bdf7624c6753393d3ecfac793861d69e
Edison Neza <edisonneza@gmail.com>
2021-02-05 16:34:41 +0100

    Delete src directory

--- src/utils/interfaces/interfaces.ts
+++
@@ -1,8 +0,0 @@
-export interface IHTMLFileType  {
-    lastModified: number,
-    lastModifiedDate: Date,
-    name: string,
-    size: number,
-    type: string,
-    webkitRelativePath: string
-};
